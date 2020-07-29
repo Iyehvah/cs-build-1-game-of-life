2ab21408908fb77da894486bb9613b97196c3b62
Iyehvah <Iyehvah@users.noreply.github.com>
2020-07-29 10:00:19 -0500

    Grid is displaying and updated with each generation. It seems to be following the rules perfectly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback, useRef} from 'react';
+import produce from 'immer';
 
-function App() {
+const numRows = 25;
+const numCols = 25;
+const neighboringCells = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [1, 1],
+  [1, -1],
+  [-1, 0],
+  [-1, 1],
+  [-1, -1]
+];
+
+const App = () => {
+  const [grid, setgrid] = useState(() => {
+    const rows = []
+    for (let i = 0; i < numRows; i ++) {
+      rows[i] = Array(numCols).fill(0);
+    }
+    return rows;
+  });
+
+  const [running, setRunning] = useState(false);
+
+  let generation = 0;
+  let generationRef = useRef(generation)
+
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    setgrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            neighboringCells.forEach(([x,y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if(newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            })
+
+            if(!g[i][k] && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            } else if (g[i][k] && neighbors >= 2 && neighbors <= 3) {
+              gridCopy[i][k] = 1;
+            } else {
+              gridCopy[i][k] = 0;
+            }
+          }
+        }
+      });
+    });
+    generationRef.current++;
+    setTimeout(runSimulation, 1000);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <button onClick={() => {
+      setRunning(!running);
+      if(!running) {
+        runningRef.current = true;
+        runSimulation();
+        generationRef.current = 0;
+      }
+      generationRef.current++
+    }}
+    >
+      {running ? "stop" : "start"}  
+    </button>
+
+    
+    <button onClick={() => {
+      if(runningRef.current) {
+        return
+      }
+      setgrid((grid) => {
+        return produce(grid, gridCopy => {
+          for(let i = 0; i < numRows; i++) {
+            for(let k = 0; k < numCols; k++) {
+              gridCopy[i][k] = 0
+            }
+          }
+        })
+      })
+      generationRef.current = 0;
+    }}>
+      clear
+    </button>
+
+
+    <div>
+      Generation: {`${generationRef.current}`}
     </div>
-  );
-}
+      <div style={{
+      display: "grid",
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+          <div
+           key={`${i}-${k}`}
+           onClick={() => {
+             const newGrid = produce(grid, gridCopy => {
+               gridCopy[i][k] =grid[i][k] ? 0 : 1;
+             })
+             setgrid(newGrid);
+           }}
+          style={{ width: 20,
+            height: 20, backgroundColor: grid[i][k] ? 'red' : undefined,
+            border: "solid 1px black"
+          }} />)
+        ))}
+      </div>
+      </>
+    );
+  }
 
 export default App;
